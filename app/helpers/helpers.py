0c116e9941a61e51c9149a769da1934116638592
O.R <aldokezer@gmail.com>
2023-01-06 09:19:45 +0100

    Created a new helper function that simplifies saving uploaed file links to DB.

--- app/helpers/helpers.py
+++ app/helpers/helpers.py
@@ -160,6 +160,13 @@ def create_guest_objects_from_form_data(request_obj, guest_model_obj, no_guests,
     return guests_to_save
 
 
+def create_uploaded_files_objects_from_form_data(request_obj, uploaded_files_model, request_id) -> list:
+    uploaded_files_model_object_list = []
+    for link in generate_s3_file_links(request_obj=request_obj, prefix=request_id):
+        uploaded_files_model_object_list.append(uploaded_files_model(link, is_signature=False, reg_id=request_id))
+    return uploaded_files_model_object_list
+
+
 # if __name__ == "__main__":
 #      # list_buckets()
 #     print(len("/Users/aldokezer/Development/morphe/app/static/uploaded_files/Monika_passport.jpeg"))
--- app/views.py
+++ app/views.py
@@ -3,7 +3,8 @@ from app.forms import RegCardForm
 from flask import render_template, request, Blueprint
 from app.helpers.data_model import website_metadata, navbar_metadata
 from app.helpers.helpers import generate_session_id, save_uploaded_files_to_s3, convert_date, \
-    radio_choice_mapper, title_selection_mapper, create_guest_objects_from_form_data, generate_s3_file_links
+    radio_choice_mapper, title_selection_mapper, create_guest_objects_from_form_data, generate_s3_file_links,\
+    create_uploaded_files_objects_from_form_data
 from app import db
 
 # for testing
@@ -18,20 +19,17 @@ def form():
     if reg_card_form.validate_on_submit():
         start_time = time.time()
         reg_id = generate_session_id(reg_card_form)
-        links = save_uploaded_files_to_s3(request, reg_id)  # save uploaded files to S3 and return links
+        save_uploaded_files_to_s3(request, reg_id)  # save uploaded files to S3 and return links
         print(generate_s3_file_links(request_obj=request, prefix=reg_id))
         reg = Registration(convert_date(reg_card_form.arrival.data),
                            convert_date(reg_card_form.departure.data),
                            reg_card_form.comment.data,
                            radio_choice_mapper(int(reg_card_form.radio.data)),
                            reg_id)
-        uploaded_file_objects = []
-        for link in links:
-            uploaded_file_objects.append(UploadedFiles(link, is_signature=False, reg_id=reg_id))
         db.session.add_all([reg])
         db.session.add_all(
             create_guest_objects_from_form_data(request, Guests, int(reg_card_form.no_guests.data), reg_id=reg_id))
-        db.session.add_all(uploaded_file_objects)
+        db.session.add_all(create_uploaded_files_objects_from_form_data(request_obj=request, uploaded_files_model=UploadedFiles, request_id=reg_id))
         db.session.commit()
         reg_card_form = RegCardForm(formdata=None)
         end_time = time.time()
