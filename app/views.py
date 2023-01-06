977ca362bc404f160e4802d6130b19be6c723aaf
O.R <aldokezer@gmail.com>
2023-01-06 10:07:10 +0100

    Form view cleanup

--- app/views.py
+++ app/views.py
@@ -3,8 +3,7 @@ from app.forms import RegCardForm
 from flask import render_template, request, Blueprint
 from app.helpers.data_model import website_metadata, navbar_metadata
 from app.helpers.helpers import generate_session_id, save_uploaded_files_to_s3, convert_date, \
-    radio_choice_mapper, title_selection_mapper, create_guest_objects_from_form_data, generate_s3_file_links,\
-    create_uploaded_file_objects_from_form_data
+    radio_choice_mapper, create_guest_objects_from_form_data, create_uploaded_file_objects_from_form_data
 from app import db
 
 # for testing
@@ -19,16 +18,21 @@ def form():
     if reg_card_form.validate_on_submit():
         start_time = time.time()
         request_id = generate_session_id(reg_card_form)
-        save_uploaded_files_to_s3(request, request_id)  # save uploaded files to S3 and return links
-        print(generate_s3_file_links(request_obj=request, prefix=request_id))
-        registration_model = Registration(convert_date(reg_card_form.arrival.data),
-                           convert_date(reg_card_form.departure.data),
-                           reg_card_form.comment.data,
-                           radio_choice_mapper(int(reg_card_form.radio.data)),
-                           request_id)
+        save_uploaded_files_to_s3(request, request_id)
+        registration_model = Registration(arrival=convert_date(reg_card_form.arrival.data),
+                                          departure=convert_date(reg_card_form.departure.data),
+                                          reservation_comment=reg_card_form.comment.data,
+                                          reservation_source=radio_choice_mapper(int(reg_card_form.radio.data)),
+                                          reg_id=request_id)
         db.session.add_all([registration_model])
-        db.session.add_all(create_guest_objects_from_form_data(request, Guests, int(reg_card_form.no_guests.data), reg_id=request_id))
-        db.session.add_all(create_uploaded_file_objects_from_form_data(request_obj=request, uploaded_files_model=UploadedFiles, request_id=request_id))
+        db.session.add_all(create_guest_objects_from_form_data(request_obj=request,
+                                                               guest_model_obj=Guests,
+                                                               no_guests=int(reg_card_form.no_guests.data),
+                                                               reg_id=request_id))
+        db.session.add_all(
+            create_uploaded_file_objects_from_form_data(request_obj=request,
+                                                        uploaded_files_model=UploadedFiles,
+                                                        request_id=request_id))
         db.session.commit()
         reg_card_form = RegCardForm(formdata=None)
         end_time = time.time()
