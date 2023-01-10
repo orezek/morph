5c6a503900a365515a8c24e2b3cc4ef586461543
O.R <aldokezer@gmail.com>
2023-01-10 16:26:00 +0100

    Refactoring code in view post_form function in order to be more reaable.

--- app/views.py
+++ app/views.py
@@ -28,21 +28,25 @@ def post_form():
         start_time = time.perf_counter()
         request_id = generate_session_id(reg_card_form)
         save_uploaded_files_to_s3(request, request_id)
-        registration_model = Registration(arrival=convert_date(reg_card_form.arrival.data),
-                                          departure=convert_date(reg_card_form.departure.data),
-                                          reservation_comment=reg_card_form.comment.data,
-                                          reservation_source=radio_choice_mapper(int(reg_card_form.radio.data)),
-                                          reg_id=request_id)
-        db.session.add_all([registration_model])
-        db.session.add_all(create_guest_objects_from_form_data(request_obj=request,
-                                                               guest_model_obj=Guests,
-                                                               no_guests=int(reg_card_form.no_guests.data),
-                                                               reg_id=request_id))
-        db.session.add_all(
-            create_uploaded_file_objects_from_form_data(request_obj=request,
-                                                        uploaded_files_model=UploadedFiles,
-                                                        request_id=request_id))
+        registration_table = [Registration(arrival=convert_date(reg_card_form.arrival.data),
+                                           departure=convert_date(reg_card_form.departure.data),
+                                           reservation_comment=reg_card_form.comment.data,
+                                           reservation_source=radio_choice_mapper(int(reg_card_form.radio.data)),
+                                           reg_id=request_id)]
+
+        guest_table = create_guest_objects_from_form_data(request_obj=request,
+                                                          guest_model_obj=Guests,
+                                                          no_guests=int(reg_card_form.no_guests.data),
+                                                          reg_id=request_id)
+
+        uploaded_file_links_table = create_uploaded_file_objects_from_form_data(request_obj=request,
+                                                                                uploaded_files_model=UploadedFiles,
+                                                                                request_id=request_id)
+        db.session.add_all(registration_table)
+        db.session.add_all(guest_table)
+        db.session.add_all(uploaded_file_links_table)
         db.session.commit()
+
         reg_card_form = RegCardForm(formdata=None)
         end_time = time.perf_counter()
         print(end_time - start_time)
